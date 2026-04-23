2ff5faf2617c1624e369edfe87b2021bc81e472a
RomanFS76 <korvy85@gmail.com>
2026-04-23 12:45:57 +0300

    feat: add cookie-parser middleware

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(
   pino({
     level: 'info',
