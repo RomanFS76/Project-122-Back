b367f805c5735125ba513e48149b24ff176bed1a
RomanFS76 <korvy85@gmail.com>
2026-04-23 13:58:33 +0300

    refactor: move logger to separate middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 // src/server.js
 import express from 'express';
-import pino from 'pino-http';
 import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
@@ -8,6 +7,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errors } from 'celebrate';
 import { errorHandler } from './middleware/errorHandler.js';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -15,26 +15,11 @@ const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello RR!' });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello RR!' });
+// });
 
 app.use(notFoundHandler);
 app.use(errors());
